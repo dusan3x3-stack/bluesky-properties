7f622777a272ff0e10a253d102de5f5c19780c8b
dusan3x3-stack <dusan3x3@gmail.com>
2026-08-25 13:31:14 +0200

    Create page.jsx

---
+++ app/admin/dodaj-oglas/page.jsx
@@ -0,0 +1,9 @@
+import DodajOglasForm from '@/components/DodajOglasForm';
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-gray-50 py-10">
+      <DodajOglasForm />
+    </main>
+  );
+}
